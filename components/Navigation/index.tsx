aec873f840757f525982c4221f8336dfb3ef6d8d
meg-engineer <cyobrin@gmail.com>
2022-02-06 19:29:59 +0900

    Navigation作成

---
+++ components/Navigation/index.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+const Navigation: React.FC = () => {
+  return (
+    <nav id="navigation">
+      <ul className="flex justify-center items-center py-8">
+        <li className="mr-12">
+          <Link href="/">
+            <a className="text-gray-500 hover:text-pink-300">HOME</a>
+          </Link>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#profile">
+            PROFILE
+          </a>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#portfolio">
+            PORTFOLIO
+          </a>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#skill">
+            SKILL
+          </a>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#contact">
+            CONTACT
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
---
+++ components/Navigation/sticky/index.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+// Types
+interface StickyNavigationProps {
+  isDisplay: boolean;
+}
+
+const StickyNavigation: React.FC<StickyNavigationProps> = (props) => {
+  const { isDisplay } = props;
+  console.log("bool", isDisplay);
+
+  return (
+    <nav
+      className={`${isDisplay ? "block sticky top-0 z-50 bg-white" : "hidden"}`}
+    >
+      <ul className="flex justify-center items-center py-8">
+        <li className="mr-12">
+          <Link href="/">
+            <a className="text-gray-500 hover:text-pink-300">HOME</a>
+          </Link>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#profile">
+            PROFILE
+          </a>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#portfolio">
+            PORTFOLIO
+          </a>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#skill">
+            SKILL
+          </a>
+        </li>
+        <li className="mr-12">
+          <a className="text-gray-500 hover:text-pink-300" href="#contact">
+            CONTACT
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default StickyNavigation;
