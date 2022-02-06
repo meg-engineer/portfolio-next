a20a336c9e62f4bcd5a8f4537b2b41ded8342ac0
meg-engineer <cyobrin@gmail.com>
2022-02-06 23:33:47 +0900

    githubリンク追加

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { portfolioList } from "app.config";
 
 // Components
@@ -6,7 +7,24 @@ import Card from "components/Card";
 const Portfolio = () => {
   return (
     <div className="py-32 text-stone-600 bg-pink-50">
-      <h1 className="mb-20 text-2xl tracking-wider text-center">PORTFOLIO</h1>
+      <h1 className="mb-12 text-2xl tracking-wider text-center">PORTFOLIO</h1>
+      <p className="mb-4 text-center">個人開発のWebアプリケーションです</p>
+      <div className="mb-4 text-center">
+        <a
+          href="https://github.com/meg-engineer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-stone-600 bg-stone-300 hover:bg-stone-50 rounded-full border-0 btn"
+        >
+          <Image
+            src="/icon/github-brands.svg"
+            width={16}
+            height={16}
+            alt="twitter"
+          />
+          <span className="ml-2 capitalize">Githubはこちら</span>
+        </a>
+      </div>
       <div className="gap-8 justify-center items-center md:flex md:px-32">
         {portfolioList.map((portfolio) => (
           <div key={portfolio.id} className="mb-3 md:mb-0">
